188d5519755c9a9b2aa8a475844fb5efc76edb66
bhixsonsimeral16 <brian.hixsonsimeral@gmail.com>
2016-02-19 12:59:27 -0600

    added a couple methods for changing the priority and goals of the bot

--- scripts/listTest.py
+++ scripts/listTest.py
@@ -1,5 +1,39 @@
 from heapq import *
 
+##on updates from twitter, checks to see if keyword is already in heapq
+##if it's in the heap it adds 1 to its value and checks to see if it is more than
+##10 above the previous goal
+##if the keyword is not in the heap it adds it with a priority of 1
+def pulledKeyword(heap, keyword, prevGoal):
+    if (keyword in (zip(*heap)[1])):
+        heap[zip(*heap)[1].index(keyword)][0] += 1
+        if ((nlargest(1, heap)[1] == keyword) && (heap[zip(*heap)[1].index(keyword)][0]) > (10 + heap[zip(*heap)[1].index(prevGoal)][0])):
+            ## make a call to change the goal
+            ## this may be difficult...
+            ## return is for setting the currentGoal
+            return (nlargest(1, heap)[1])
+    else:
+        lst = [1, keyword]
+        heappush (heap, lst)
+
+## Called when the goal has been reached.  Sets the priority to 0
+## Then creates new goal
+def goalReached(heap, prevGoal):
+    if (prevGoal in (zip(*heap)[1])):
+        heap[zip(*heap)[1].index(prevGoal)][0] = 0
+        heap.sort
+        if ((nlargest(1, heap)[0] > 0)):
+            ## make a function call to create a new goal
+            ## nlargest(1, heap)[1] is the keyword that becomes the new goal
+            ## return is for setting the currentGoal
+            return (nlargest(1, heap)[1])
+        else:
+            ## return is for setting the currentGoal
+            ## "stop" is the natural state of the twitterBot
+            return("stop")
+
+## currentGoal maintains what the each new goal will be checked against
+currentGoal = "stop"
 priority = []
 
 l1 = [1, 'forward']
