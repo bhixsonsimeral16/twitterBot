092388e0d83780436b1b3dee53d0fa581bc920ae
bhixsonsimeral16 <brian.hixsonsimeral@gmail.com>
2016-02-21 15:44:58 -0600

    Made the code compatible with python 3. Did some tests with lists

--- scripts/listTest.py
+++ scripts/listTest.py
@@ -7,7 +7,7 @@ from heapq import *
 def pulledKeyword(heap, keyword, prevGoal):
     if (keyword in (zip(*heap)[1])):
         heap[zip(*heap)[1].index(keyword)][0] += 1
-        if ((nlargest(1, heap)[1] == keyword) && (heap[zip(*heap)[1].index(keyword)][0]) > (10 + heap[zip(*heap)[1].index(prevGoal)][0])):
+        if ((nlargest(1, heap)[1] == keyword) & (heap[zip(*heap)[1].index(keyword)][0]) > (10 + heap[zip(*heap)[1].index(prevGoal)][0])):
             ## make a call to change the goal
             ## this may be difficult...
             ## return is for setting the currentGoal
@@ -46,11 +46,16 @@ heappush (priority,l2)
 heappush (priority,l3)
 heappush (priority,l4)
 
-print (priority[zip(*priority)[1].index('forward')][0])
+#the *priority unzips the heap, so that there are 4 sepearate lists
+#instead of whatever they are stored as by the heap
+#this does not work with python 3.4, only with 2.7
+#zips are not printable or subscriptable in 3.4
+#in 3 I will need to add a list() command to change the iterator into a list
+print (priority[list(zip(*priority))[1].index('forward')][0])
 
-priority[zip(*priority)[1].index('forward')][0] += 1
+priority[list(zip(*priority))[1].index('forward')][0] += 1
 
-print (priority[zip(*priority)[1].index('forward')][0])
+print (priority[list(zip(*priority))[1].index('forward')][0])
 
 print (nlargest(4, priority))
 
