129b4ee2e5efca3ce7e33a0dbd32053650310dec
Brian Hixson-Simeral <brian.hixsonsimeral@gmail.com>
2016-02-25 14:34:39 -0600

    cleaned up whitespace

--- twitterBot_ws/src/twitterBot/src/tugOfWar.py
+++ twitterBot_ws/src/twitterBot/src/tugOfWar.py
@@ -15,25 +15,25 @@ from sensor_msgs.msg import LaserScan
 position = 1.5
 
 if __name__ == '__main__':
-	global position
-	rospy.init_node('tug')
+    global position
+    rospy.init_node('tug')
 
-	# A publisher for the move data
+    # A publisher for the move data
     pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist)
 	
-	command = twist()
+    command = twist()
 
     command.linear.x = 0.0
-	command.linear.y = 0.0
+    command.linear.y = 0.0
     command.linear.z = 0.0
     command.angular.x = 0.0
     command.angular.y = 0.0
     command.angular.z = 0.0
 
     # take the strings from the 'tweets' topic and push them to the callback method
-	currentGoal = rospy.Subscriber('tweets', StdMsgs, callback)
+    currentGoal = rospy.Subscriber('tweets', StdMsgs, callback)
 
-	# Loop at 10Hz, publishing movement commands until we shut down.
+    # Loop at 10Hz, publishing movement commands until we shut down.
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         pub.publish(command)
